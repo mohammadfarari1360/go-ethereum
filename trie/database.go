f7601e5a049a1e01f9f342fab663d5a6d235876d
Guillaume Ballet <gballet@gmail.com>
2020-08-26 09:47:02 +0200

    add stackTrieGenerate

--- trie/database.go
+++ trie/database.go
@@ -105,6 +105,14 @@ type rawNode []byte
 
 func (n rawNode) cache() (hashNode, bool)   { panic("this should never end up in a live trie") }
 func (n rawNode) fstring(ind string) string { panic("this should never end up in a live trie") }
+func (n rawNode) EncodeRLP(w io.Writer) error {
+	count, err := w.Write([]byte(n))
+	if err != nil && count != len(n) {
+		err = fmt.Errorf("rawNode: could not write full length")
+	}
+
+	return err
+}
 
 // rawFullNode represents only the useful data content of a full node, with the
 // caches and flags stripped out to minimize its data storage. This type honors
@@ -206,7 +214,7 @@ func forGatherChildren(n node, onChild func(hash common.Hash)) {
 		}
 	case hashNode:
 		onChild(common.BytesToHash(n))
-	case valueNode, nil:
+	case valueNode, nil, rawNode:
 	default:
 		panic(fmt.Sprintf("unknown node type: %T", n))
 	}
