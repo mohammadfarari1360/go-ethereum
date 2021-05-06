6a1f344c59bea9ceb0f874a61fbb11b0e55d5b03
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-05-06 18:58:39 +0200

    all: implement EIP-compliant verkle trees
    verkle: Implement Trie, NodeIterator and Database ifs
    
    Fix crash in TestDump
    
    Fix TestDump
    
    Fix TrieCopy
    
    remove unnecessary traces
    
    fix: Error() returned errIteratorEnd in verkle node iterator
    
    rewrite the iterator and change the signature of OpenStorageTrie
    
    add the adapter to reuse the account trie for storage
    
    don't try to deserialize a storage leaf into an account
    
    Fix statedb unit tests (#14)
    
    * debug code
    
    * Fix more unit tests
    
    * remove traces
    
    * Go back to the full range
    
    One tree to rule them all
    
    remove updateRoot, there is no root to update
    
    store code inside the account leaf
    
    fix build
    
    save current state for Sina
    
    Update go-verkle to latest
    
    Charge WITNESS_*_COST gas on storage loads
    
    Add witness costs for SSTORE as well
    
    Charge witness gas in the case of code execution
    
    corresponding code deletion
    
    add a --verkle flag to separate verkle experiments from regular geth operations
    
    use the snapshot to get data
    
    stateless execution from block witness
    
    AccessWitness functions
    
    Add block generation test + genesis snapshot generation
    
    test stateless block execution (#18)
    
    * test stateless block execution
    
    * Force tree resolution before generating the proof
    
    increased coverage in stateless test execution (#19)
    
    * test stateless block execution
    
    * Force tree resolution before generating the proof
    
    * increase coverage in stateless test execution
    
    ensure geth compiles
    
    fix issues in tests with verkle trees deactivated
    
    Ensure stateless data is available when executing statelessly (#20)
    
    * Ensure stateless data is available when executing statelessly
    
    * Actual execution of a statless block
    
    * bugfixes in stateless block execution
    
    * code cleanup
    
     - Reduce PR footprint by reverting NewEVM to its original signature
     - Move the access witness to the block context
     - prepare for a change in AW semantics
       Need to store the initial values.
     - Use the touch helper function, DRY
    
    * revert the signature of MustCommit to its original form (#21)
    
    fix leaf proofs in stateless execution (#22)
    
    * Fixes in witness pre-state
    
    * Add the recipient's nonce to the witness
    
    * reduce PR footprint and investigate issue in root state calculation
    
    * quick build fix
    
    cleanup: Remove extra parameter in ToBlock
    
    revert ToBlock to its older signature
    
    fix import cycle in vm tests
    
    fix linter issue
    
    fix appveyor build
    
    fix nil pointers in tests
    
    Add indices, yis and Cis to the block's Verkle proof
    
    upgrade geth dependency to drop geth's common dep
    
    fix cmd/devp2p tests
    
    fix rebase issues
    
    quell an appveyor warning
    
    fix address touching in SLOAD and SSTORE
    
    fix access witness for code size
    
    touch target account data before calling
    
    make sure the proper locations get touched in (ext)codecopy
    
    touch all code pages in execution
    
    add pushdata to witness
    
    remove useless code in genesis snapshot generation
    
    testnet: fix some of the rebase/drift issues
    
    Fix verkle proof generation in block
    
    fix an issue occuring when chunking past the code size
    
    fix: ensure the code copy doesn't extend past the code size

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -156,6 +156,7 @@ var (
 		utils.MinerNotifyFullFlag,
 		configFileFlag,
 		utils.CatalystFlag,
+		utils.VerkleFlag,
 	}
 
 	rpcFlags = []cli.Flag{
--- cmd/geth/usage.go
+++ cmd/geth/usage.go
@@ -229,6 +229,7 @@ var AppHelpFlagGroups = []flags.FlagGroup{
 			utils.BloomFilterSizeFlag,
 			cli.HelpFlag,
 			utils.CatalystFlag,
+			utils.VerkleFlag,
 		},
 	},
 }
