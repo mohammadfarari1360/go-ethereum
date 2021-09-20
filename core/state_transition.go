628c10ed7b3fe3e2c4975fe0329226bbdb4135a4
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-09-20 20:56:37 +0200

    Fixes in witness pre-state

--- core/state_transition.go
+++ core/state_transition.go
@@ -17,6 +17,7 @@
 package core
 
 import (
+	"encoding/binary"
 	"fmt"
 	"math"
 	"math/big"
@@ -293,12 +294,16 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	if st.evm.TxContext.Accesses != nil {
 		if msg.To() != nil {
 			toBalance := trieUtils.GetTreeKeyBalance(*msg.To())
-			gas += st.evm.TxContext.Accesses.TouchAddressAndChargeGas(toBalance)
+			pre := st.state.GetBalance(*msg.To())
+			gas += st.evm.TxContext.Accesses.TouchAddressAndChargeGas(toBalance, pre.Bytes())
 		}
 		fromBalance := trieUtils.GetTreeKeyBalance(msg.From())
+		preFB := st.state.GetBalance(msg.From()).Bytes()
 		fromNonce := trieUtils.GetTreeKeyNonce(msg.From())
-		gas += st.evm.TxContext.Accesses.TouchAddressAndChargeGas(fromNonce)
-		gas += st.evm.TxContext.Accesses.TouchAddressAndChargeGas(fromBalance)
+		var preFN [8]byte
+		binary.BigEndian.PutUint64(preFN[:], st.state.GetNonce(msg.From()))
+		gas += st.evm.TxContext.Accesses.TouchAddressAndChargeGas(fromNonce, preFN[:])
+		gas += st.evm.TxContext.Accesses.TouchAddressAndChargeGas(fromBalance, preFB[:])
 	}
 	st.gas -= gas
 
