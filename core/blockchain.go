19daa12678fec73068a8f606fd75343d108fe1d8
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-11-26 15:37:36 +0100

    Remove access witness from the signature of Process

--- core/blockchain.go
+++ core/blockchain.go
@@ -1614,7 +1614,7 @@ func (bc *BlockChain) insertChain(chain types.Blocks, verifySeals bool) (int, er
 			receipts types.Receipts
 			logs     []*types.Log
 		)
-		receipts, logs, _, usedGas, err = bc.processor.Process(block, statedb, bc.vmConfig)
+		receipts, logs, usedGas, err = bc.processor.Process(block, statedb, bc.vmConfig)
 		if err != nil {
 			bc.reportBlock(block, receipts, err)
 			atomic.StoreUint32(&followupInterrupt, 1)
