fec873e3289086abbe906649a963ca9a97206de8
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2022-06-08 15:51:27 +0000

    actually use the function

--- trie/utils/verkle.go
+++ trie/utils/verkle.go
@@ -185,7 +185,7 @@ func getTreeKeyWithEvaluatedAddess(evaluated *verkle.Point, treeIndex *uint256.I
 
 }
 
-func evaluateAddressPoint(address []byte) *verkle.Point {
+func EvaluateAddressPoint(address []byte) *verkle.Point {
 	if len(address) < 32 {
 		var aligned [32]byte
 		address = append(aligned[:32-len(address)], address...)
